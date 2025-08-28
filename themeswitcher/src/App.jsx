9de522e9cd5fa07e4f425f5a03c0661e01c389ff
Manas Kumar Dutta <manas5226j@gmail.com>
2025-08-28 23:00:48 +0530

    Theme changing effect added with button uses useContext hook

---
+++ themeswitcher/src/App.jsx
@@ -0,0 +1,37 @@
+import React,{ useEffect } from "react";
+import "./App.css";
+import { ThemeProvider } from "./context/Theme";
+import Button from "./components/Button";
+import Card from "./components/Card";
+
+function App() {
+const [themeMode,setThemeMode]=React.useState("light");
+const darkTheme=()=>{
+  setThemeMode("dark");
+}
+const lightTheme=()=>{
+  setThemeMode("light");
+}
+useEffect(()=>{
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
+  return (
+    <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <Button />
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          <Card />
+        </div>
+      </div>
+    </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
